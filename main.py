e76780b7788b4daf9290d63852d0d6ad4a1a6d6f
Noia <Emanuele.Noia1@tesco.com>
2023-02-07 17:56:33 +0000

    plugged in flask and tabulator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+from flaskwebgui import FlaskUI
 app = Flask(__name__)
 
 
@@ -8,8 +8,11 @@ def index():
     if request.method == "POST":
         code = request.form.get("code")
         # Do something with the code here, such as saving it to a file or processing it further
-    return render_template("ace.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
+    # If you are debugging you can do that in the browser:
     app.run()
+    # If you want to view the flaskwebgui window:
+    #FlaskUI(app=app, server="flask").run()
